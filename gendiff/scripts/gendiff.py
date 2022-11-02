bf986f5c0f678426749720db86b342ffb34d21a8
Евгения Ольшанова <olshanova1995@gmail.com>
2022-11-02 13:14:23 +0300

    Первый шаг

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Gendiff script."""
+
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.',
+    )
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
+    args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    main()
