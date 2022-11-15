d2eb1a4f4bd141ddd402682c7e046abe758bb8e1
Евгения Ольшанова <olshanova1995@gmail.com>
2022-11-15 19:35:20 +0300

    Вторые изменения после третьей проверки (parse.py и generate.diff)

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,12 +1,10 @@
-from gendiff.parse import get_content_and_extension, parse
 from gendiff.diff import diff
+from gendiff.parse import get_content_and_extension
 from gendiff.formatters.use_formatter import apply_format
 
 
 def generate_diff(path1, path2, format='stylish'):
-    data1, format1 = get_content_and_extension(path1)
-    data2, format2 = get_content_and_extension(path2)
-    dict1 = parse(data1, format1)
-    dict2 = parse(data2, format2)
-    result = diff(dict1, dict2)
+    data1 = get_content_and_extension(path1)
+    data2 = get_content_and_extension(path2)
+    result = diff(data1, data2)
     return apply_format(result, format)
--- gendiff/parse.py
+++ gendiff/parse.py
@@ -3,15 +3,14 @@ import yaml
 import json
 
 
+def get_content_and_extension(path):
+    with open(path, "r") as data:
+        return parse(data.read(), Path(path).suffix[1:])
+
+
 def parse(data, format: str):
     if format == 'json':
-        return json.load(data)
+        return json.loads(data)
     if format == 'yaml' or format == 'yml':
         return yaml.safe_load(data)
     raise Exception(f"No such method for format: {format}")
-
-
-def get_content_and_extension(path):
-    format = Path(path).suffix[1:]
-    data = open(path)
-    return data, format
