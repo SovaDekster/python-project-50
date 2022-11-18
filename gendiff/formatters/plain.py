78abd569332ae0eb6d374d3b5d8e48e52ed2adc0
Евгения Ольшанова <olshanova1995@gmail.com>
2022-11-18 11:49:30 +0300

    Поменяла plain.py, на очереди stylish

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -10,23 +10,23 @@ def to_str(value):
     return f"'{value}'"
 
 
+def walk(node, path=''):
+    result = []
+    for k, v in node.items():
+        current_path = f"{path}{v['key']}"
+        start_line = f"Property '{current_path}'"
+        if v['operation'] == 'changed':
+            result.append(f"{start_line} was updated. "
+                          f"From {to_str(v['old'])} to {to_str(v['new'])}")
+        if v['operation'] == 'nested':
+            result.append(walk(v['value'], current_path + '.'))
+        if v['operation'] == 'removed':
+            result.append(f"{start_line} was removed")
+        if v['operation'] == 'added':
+            result.append(f"{start_line} was added "
+                          f"with value: {to_str(v['value'])}")
+    return '\n'.join(result)
+
+
 def plain_format(diff_result: dict):
-
-    def walk(node, path=''):
-        result = []
-        for k, v in node.items():
-            current_path = f"{path}{v['key']}"
-            start_line = f"Property '{current_path}'"
-            if v['operation'] == 'changed':
-                result.append(f"{start_line} was updated. "
-                              f"From {to_str(v['old'])} to {to_str(v['new'])}")
-            if v['operation'] == 'nested':
-                result.append(walk(v['value'], current_path + '.'))
-            if v['operation'] == 'removed':
-                result.append(f"{start_line} was removed")
-            if v['operation'] == 'added':
-                result.append(f"{start_line} was added "
-                              f"with value: {to_str(v['value'])}")
-        return '\n'.join(result)
-
     return walk(diff_result)
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -21,24 +21,25 @@ def build_line(data, key, depth, indent='  '):
            f"{value_to_str(data[key], depth + 1)}"
 
 
-def stylish_format(diff_result):
+def walk(node, depth=0, replacer='  ', indent='    '):
+    lines = []
+    space = replacer * (depth + 1)
+    for k, v in node.items():
+        if v['operation'] == 'nested':
+            lines.append(f"{space * 2}{v['key']}: "
+                         f"{walk(v['value'], depth + 1)}")
+        if v['operation'] == 'unchanged':
+            lines.append(f"{space}{build_line(v, 'value', depth)}")
+        if v['operation'] == 'changed':
+            lines.append(f"{space}{build_line(v, 'old', depth, '- ')}")
+            lines.append(f"{space}{build_line(v, 'new', depth, '+ ')}")
+        if v['operation'] == 'removed':
+            lines.append(f"{space}{build_line(v, 'value', depth, '- ')}")
+        if v['operation'] == 'added':
+            lines.append(f"{space}{build_line(v, 'value', depth, '+ ')}")
+    result = itertools.chain('{', lines, [indent * depth + '}'])
+    return "\n".join(result)
 
-    def walk(node, depth=0, replacer='  ', indent='    '):
-        lines = []
-        space = replacer * (depth + 1)
-        for k, v in node.items():
-            if v['operation'] == 'nested':
-                lines.append(f"{space * 2}{v['key']}: "
-                             f"{walk(v['value'], depth + 1)}")
-            if v['operation'] == 'unchanged':
-                lines.append(f"{space}{build_line(v, 'value', depth)}")
-            if v['operation'] == 'changed':
-                lines.append(f"{space}{build_line(v, 'old', depth, '- ')}")
-                lines.append(f"{space}{build_line(v, 'new', depth, '+ ')}")
-            if v['operation'] == 'removed':
-                lines.append(f"{space}{build_line(v, 'value', depth, '- ')}")
-            if v['operation'] == 'added':
-                lines.append(f"{space}{build_line(v, 'value', depth, '+ ')}")
-        result = itertools.chain('{', lines, [indent * depth + '}'])
-        return "\n".join(result)
+
+def stylish_format(diff_result):
     return walk(diff_result)
